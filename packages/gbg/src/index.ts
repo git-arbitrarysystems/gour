55aff8b55653cb533ae11ffd7d3f838703d94624
Jasper Funk-smit <^_^@arbitrarysystems.com>
2023-12-08 13:51:40 +0100

    added webpack

--- packages/gbg/src/index.ts
+++ packages/gbg/src/index.ts
@@ -4,9 +4,10 @@ import { Board } from "./Board";
 import { Player } from "./Player";
 import { Tile } from "./Tile";
 import { AI } from "./AI";
-import { State } from "./State";
 
+const game = new Game()
+console.dir(game)
 
 export {
-    Game, Board, Player, Tile, AI, State
+    Game, Board, Player, Tile, AI
 }
---
+++ packages/gbg/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'), // Set the correct path to your static files
+      },
+      compress: true,
+      port: 8080,
+  },
+};
